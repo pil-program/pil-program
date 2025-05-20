1034ee33f36f4e2c9db2ad5bed015407fb261e27
charugundla <charugundlavipul@gmail.com>
2025-05-20 11:11:55 -0400

    media query changes

--- src/components/AboutUsSection.jsx
+++ src/components/AboutUsSection.jsx
@@ -39,22 +39,7 @@ export default function AboutUsSection() {
         <div className="stats-section">
           <div className="stats-header">
             <h2 className="stats-number">15,000+</h2>
-            {/* <svg
-              className="arrow-icon"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7 17L17 7M17 7H7M17 7V17"
-                stroke="#0C2852"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg> */}
+            
           </div>
           <p className="stats-description">
            Hours of everyday stress saved 
